d78d941b7855be461bf7a1c5e6b51155db924e53
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-11-16 13:53:37 -0500

    Finished forward kin code but untested

--- rbe500_scara_kinematics/src/forwardkinematics.py
+++ rbe500_scara_kinematics/src/forwardkinematics.py
@@ -1,37 +1,70 @@
 #!/usr/bin/env python
 import math
-from rbe500_scara_kinematics.msg import qVal
+from rbe500_scara_kinematics.srv import forwardkinService, forwardkinServiceResponse
 import rospy
 
 class FK:
     def __init__(self):
         self._getQVals = rospy.Subscriber('/qVals', qVal, self.doForwardKin, queue_size=1)
 
+    def server(self):
+        serviceFK = rospy.Service("ScaraFK", forwardkinService, self.doForwardKin)
+
     def doForwardKin(self, data):
         q1 = math.radians(data.q1)
         q2 = math.radians(data.q2)
-        q3 = math.radians(data.q3)
-        print("Output Matrix")
-        print(math.cos(q2+q3)*math.cos(q1)),
-        print(-math.sin(q2+q3)*math.cos(q1)),
-        print(-math.sin(q1)),
-        print(math.cos(q1)*math.cos(q2)-2*math.sin(q1)+math.cos(q1)*math.cos(q2)*math.cos(q3)-math.cos(q1)*math.sin(
-            q2)*math.sin(q3))
+        q3 = data.q3
+        # This code calculates each value of the FK
+        # using equations found in matlab, also included in the submission
+        row11 = math.cos(q1) * math.cos(q2) - math.sin(q1) * math.sin(q2)
+        row12 = -math.cos(q1) * math.sin(q2) - math.cos(q2) * math.sin(q1)
+        row13 = 0
+        row14 = (math.cos(q1) + math.cos(q1) * math.cos(q2) - math.sin(q1) * math.sin(q2)) / 5
 
-        print(math.cos(q2 + q3) * math.sin(q1)),
-        print(-math.sin(q2 + q3) * math.sin(q1)),
-        print(-math.cos(q1)),
-        print(math.sin(q1) * math.cos(q2) + 2 * math.cos(q1) + math.sin(q1) * math.sin(q2) * math.sin(q3) - math.cos(
-            q3) * math.cos(q2) * math.sin(q1))
-        print(-math.sin(q2+q3)),
-        print(-math.cos(q2+q3)),
-        print(0),
-        print(1-math.sin(q2)-math.sin(q2+q3))
+        row21 = math.cos(q1) * math.sin(q2) + math.cos(q2) * math.sin(q1)
+        row22 = math.cos(q1) * math.cos(q2) - math.sin(q1) * math.sin(q2)
+        row23 = 0
+        row24 = (math.sin(q1) + math.cos(q1) * math.sin(q2) + math.sin(q1) * math.cos(q2)) / 5
 
-        print(0),
-        print(0),
-        print(0),
-        print(1)
+        row31 = 0
+        row32 = 0
+        row33 = 1
+        row34 = q3 + 1 / 10
+
+        row41 = 0
+        row42 = 0
+        row43 = 0
+        row44 = 1
+
+        x = row14
+        y = row24
+        z = row34
+
+        psi = 0
+        theta = 0
+        phi = 0
+        forceOrientation = 1
+
+        if(row31 != 1):
+
+            if(forceOrientation):
+                theta = -math.asin(row31)
+            else:
+                theta = math.pi-theta
+
+            psi = math.atan2(row32 / math.cos(theta), row33 / math.cos(theta))
+            phi = math.atan2(row21 / math.cos(theta), row11 / math.cos(theta))
+
+        else:
+            phi = 0
+            if(row31 == -1):
+                theta = math.pi/2
+                psi = phi + math.atan2(row12, row13)
+            else:
+                theta = -math.pi/2
+                psi = -phi + math.atan2(-row12, -row13)
+
+        return forwardkinServiceResponse(x, y, z, phi, theta, psi)
 
 
 
