f6c0859be0381cdf946d6e6b883402281e4f7e9d
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-11-17 17:23:30 -0500

    Got control of robot through gui

--- rbe500_scara_control/src/ProjectP2ControllerUI.py
+++ rbe500_scara_control/src/ProjectP2ControllerUI.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QSizePolicy
 import sys
+from std_msgs.msg import Float64
 
-designerFile = "RBE500ProjP2UI.ui"
+designerFile = "/media/nick/LinuxStorage/Documents/RBE500/catkin_ws/src/RBE500Project/rbe500_scara_control/src/RBE500ProjP2UI.ui"
 class P2Gui(QtWidgets.QMainWindow):
     def __init__(self):
         super(P2Gui, self).__init__()
         self.ui = uic.loadUi(designerFile, self)
+        self.J3Pub = rospy.Publisher('/scara/joint3_position_controller/command', Float64, queue_size=1)
+        self.ZPositionBTN.clicked.connect(self.sendZPos)
+
+    def sendZPos(self):
+        zPos = self.ZPosEntry.text()
+        try:
+            zPos = float(zPos)
+        except Exception as e:
+            self.ZPosEntry.setText("0")
+            zPos = 0.0
+        self.J3Pub.publish(zPos)
+
+
+
+
+
 
 if __name__ == '__main__':
-    rospy.init_node('CommNode')
+    rospy.init_node('Part2Controller')
     rospy.sleep(1)
     app = QApplication(sys.argv)
     window = P2Gui()
