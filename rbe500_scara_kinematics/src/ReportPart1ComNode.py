74f93a8b0ea1fdc966f78a4d6a27a4ba1e383292
Nicholas Lanotte <Njlanotte@wpi.edu>
2019-11-20 22:13:08 -0500

    Added some comments

--- rbe500_scara_kinematics/src/ReportPart1ComNode.py
+++ rbe500_scara_kinematics/src/ReportPart1ComNode.py
@@ -22,6 +22,7 @@ class part1ComNode:
         self.currentRobotQ3 = 0
 
     def updateRobotState(self, data):
+        #This constantly updates this nodes known position of the robot
 
         if(self.useRviz):
             self.currentRobotQ1 = data.position[0]
@@ -32,7 +33,9 @@ class part1ComNode:
             self.currentRobotQ2 = data.position[1]
             self.currentRobotQ3 = data.position[0]
 
+    #runs everytime the user presses a button
     def checkKinematics(self):
+        #stores current robot position
         tempq1 = round(self.currentRobotQ1, 3)
         tempq2 = round(self.currentRobotQ2, 3)
         tempq3 = round(self.currentRobotQ3, 3)
@@ -41,8 +44,10 @@ class part1ComNode:
         print("q2 = " + str(tempq2))
         print("q3 = " + str(tempq3))
 
+        #sends them to fk service
         sendFK = rospy.ServiceProxy('ScaraFK', forwardkinService)
         resp1 = sendFK(tempq1, tempq2, tempq3)
+        #retrives fk from response
         x = resp1.x
         y = resp1.y
         z = resp1.z
@@ -57,7 +62,7 @@ class part1ComNode:
         print("theta = " + str(theta))
         print("phi = " + str(phi))
 
-
+        #send fk to ik node in both elbow positions to have both possible positions known
         sendIK = rospy.ServiceProxy('ScaraIK', inversekinService)
         resp2 = sendIK(x, y, z, psi, theta, phi, 1)
         resp3 = sendIK(x, y, z, psi, theta, phi, 0)
@@ -70,14 +75,14 @@ class part1ComNode:
         q31 = round(resp3.q3, 3)
 
 
-
+        #checks to see if the result from the ik equals the original values when elbow = 1
         if(q1==tempq1 and q2 == tempq2 and q3==tempq3):
             print("Inverse Kinematics Results")
             print("Joint Positions match in the elbow left position")
             print("q1 = " + str(q1))
             print("q2 = " + str(q2))
             print("q3 = " + str(q3))
-
+        #checks to see if the result from the ik equals the original values when elbow = 0
         elif (q11 == tempq1 and q21 == tempq2 and q31 == tempq3):
             print("Inverse Kinematics Results")
             print("Joint Positions match in the elbow right position")
@@ -90,7 +95,7 @@ class part1ComNode:
             print("q1 = " + str(q1))
             print("q2 = " + str(q2))
             print("q3 = " + str(q3))
-            print("Elboe right")
+            print("Elbow right")
             print("q1 = " + str(q11))
             print("q2 = " + str(q21))
             print("q3 = " + str(q31))
